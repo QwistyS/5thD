0ea0f9ded118e37c8ad7ebcda55aea6e04090a6c
Daniel Mor <onecoolterran@gmail.com>
2024-06-05 15:29:23 +0300

    Spdlog and peer

--- core/inc/thread_pool.h
+++ core/inc/thread_pool.h
@@ -1,31 +1,34 @@
 // thread_pool.h
+
 #ifndef THREAD_POOL_H
 #define THREAD_POOL_H
 
-#include <vector>
+#include <functional>
 #include <queue>
 #include <thread>
+#include <vector>
 #include <mutex>
 #include <condition_variable>
-#include <functional>
-#include <atomic>
 
 class ThreadPool {
 public:
     ThreadPool(size_t numThreads);
     ~ThreadPool();
 
-    void enqueueTask(const std::function<void()>& task);
+    template<class F, class... Args>
+    void enqueue(F&& f, Args&&... args);
 
 private:
+    // Need to keep track of threads so we can join them
     std::vector<std::thread> workers;
+
+    // The task queue
     std::queue<std::function<void()>> tasks;
 
+    // Synchronization
     std::mutex queueMutex;
     std::condition_variable condition;
-    std::atomic<bool> stop;
-
-    void workerThread();
+    bool stop;
 };
 
-#endif // THREAD_POOL_H
+#endif /* THREAD_POOL_H */
